2a320804cae4d5d135b3800cd88a088524008528
Magnus <zelvekalois@caramail.fr>
2020-10-05 13:53:32 +0200

    Update docs

--- docusaurus/website/pages/en/index.js
+++ docusaurus/website/pages/en/index.js
@@ -96,15 +96,6 @@ class Index extends React.Component {
         </Container>
     );
 
-    const FeatureCallout = () => (
-        <div
-            className="productShowcaseSection paddingBottom"
-            style={{textAlign: 'center'}}>
-          <h2>Feature Callout</h2>
-          <MarkdownBlock>These are features of this project</MarkdownBlock>
-        </div>
-    );
-
     const TryOut = () => (
         <Block id="try">
           {[
@@ -213,7 +204,6 @@ class Index extends React.Component {
           <HomeSplash siteConfig={siteConfig} language={language}/>
           <div className="mainContainer">
             <Features/>
-            <FeatureCallout/>
             <LearnHow/>
             <TryOut/>
             <Description/>
